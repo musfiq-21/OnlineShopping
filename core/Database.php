df49f36bc51646efb8f3790a0d2656e2ae70a176
musfiq-21 <bsse1521@iit.du.ac.bd>
2026-03-04 12:05:36 +0600

    add database singleton connection

---
+++ core/Database.php
@@ -0,0 +1,46 @@
+<?php
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+class Database
+{
+    
+
+    private static $connection = null;
+
+    private function __construct()
+    {
+        static $host = "localhost";
+        static $dbname = $_ENV['dbname'];
+        static $username = "root";
+        static $password = $_ENV['password'];
+
+
+        if (self::$connection === null) {
+            try {
+                self::$connection = new PDO(
+                    "mysql:host=" . $host . ";dbname=" . $dbname . ";charset=utf8mb4",
+                    $username,
+                    $password,
+                    [
+                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                        PDO::ATTR_EMULATE_PREPARES => false
+                    ]
+                );
+            } catch (PDOException $e) {
+                die("Database connection failed: " . $e->getMessage());
+            }
+        }
+    }
+
+    public static function getConnection()
+    {
+        if (self::$connection === null) {
+            self::$connection = new self();
+        }
+        return self::$connection;
+    }
+}
